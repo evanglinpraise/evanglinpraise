096eb31abfd4132cf4b8827d647a07d34c8cc420
evanglinpraise <evanglinyobu@gmail.com>
2016-12-22 23:05:49 -0800

    Add files via upload

---
+++ positive.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+  int n;
+  printf("\n enter the number :");
+  scanf("%d",&n);
+  if(n<0){
+    printf("the value is negative");
+  }
+  if(n=0)
+    {
+  printf("\n it is the boundary condition");
+  }
+  if(n>0)
+    {
+  printf("the value is positive");
+  }
+
+}
