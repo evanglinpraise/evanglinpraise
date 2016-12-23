1d4304f18426dd178b13b2ff33aea7af6391d6e0
evanglinpraise <evanglinyobu@gmail.com>
2016-12-23 01:08:48 -0800

    Add files via upload

---
+++ odd1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the no :");
+    scanf("%d",&n);
+    if(n%2==0){
+        printf("\n the no is even");
+    }else{
+    printf("the num is odd");
+    }
+    return 0;
+}
