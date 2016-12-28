dd7b677039ea331b4a6754d8bed943ed68431912
evanglinpraise <evanglinyobu@gmail.com>
2016-12-27 21:00:20 -0800

    Add files via upload

---
+++ mins1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+#define min();
+int main()
+{
+    int h1,h2,min=0,diff;
+    printf("enter the hour1:");
+    scanf("\n %d",&h1);
+    printf("enter the hour2:");
+    scanf("\n %d",&h2);
+    if(h1!=h2)
+    {
+       dif=h1-h2;
+       printf("the diff is %d",diff);
+    }
+    else
+    {
+       printf("it is  a zero");
+    }
+int min()
+        {
+            min=diff*60;
+
+        }
+        printf("\n the min is: %d",min);
+
+       return 0;
+
+}
