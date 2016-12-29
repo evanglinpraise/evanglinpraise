547ebff8b9a3c7bb8775d961f4f3d148ff0b9636
evanglinpraise <evanglinyobu@gmail.com>
2016-12-28 19:56:11 -0800

    Add files via upload

---
+++ woeking.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    printf("\n enter the day");
+    scanf("%s",&str[100]);
+    if(strcmp(str[100],"monday"==0)||(strcmp(str[100],"tuesday")==0)||(strcmp(str[100],"wednesday")==0)||(strcmp(str[100],"thursday")==0)||(strcmp(str[100],"friday")==0)
+    {
+        printf("true");
+    }
+    else
+    {
+        printf("false");
+    }
+    return 0;
+}
