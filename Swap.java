643117974adbec9cb691875755f183fa65b49838
evanglinpraise <evanglinyobu@gmail.com>
2017-07-04 15:19:08 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,15 @@
+package begginer;
+
+public class Swap {
+
+	public static void main(String[] args) {
+		int a=67;
+		int b=89;
+		int c;
+		c=a;
+		a=b;
+		b=c;
+		System.out.println(a+" "+b);
+	}
+
+}
