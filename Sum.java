d0c00624f206ba74e63e043ecf8f50f8d12bbac5
evanglinpraise <evanglinyobu@gmail.com>
2017-07-04 15:13:12 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,21 @@
+package begginer;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		int sum1=0;
+		int sum2=0;
+		for(int i=1;i<=15;i++){
+			sum1=sum1+i;
+		}
+		System.out.println(sum1);
+		for(int i=15;i<=45;i++){
+			if(i%2!=0){
+				sum2=sum2+i;
+			}
+		}
+		System.out.println(sum2);
+
+	}
+
+}
