5dddf3b3b2c2698d2b20bca0009db9a7e4d4d449
evanglinpraise <evanglinyobu@gmail.com>
2017-07-05 12:46:39 +0530

    Add files via upload

---
+++ Hunter96.java
@@ -0,0 +1,14 @@
+package begginer;
+
+public class Hunter96 {
+
+	public static void main(String[] args) {
+		String str="java";
+		char ch1=(char)(str.charAt(0)-1);
+		char ch2=(char)(str.charAt(1)+25);
+		char ch3=(char)(str.charAt(2)-1);
+		char ch4=(str.charAt(3));
+		System.out.println(ch1+""+ch2+""+ch3+""+ch4);
+	}
+
+}
